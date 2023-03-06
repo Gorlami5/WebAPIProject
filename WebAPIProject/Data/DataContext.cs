8d72482f76fac1797e1bc83937688ed94938873b
Muhammet Yonkuç <muhammetyonkuc99@gmail.com>
2023-03-06 22:34:06 +0300

    Proje dosyası ekle.

---
+++ WebAPIProject/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebAPIProject.Models;
+
+namespace WebAPIProject.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+        public City Cities { get; set; }
+
+        public Photo Photos { get; set; }
+
+        public User Users { get; set; }
+
+    }
+}
---
+++ WebAPIProject/Models/City.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPIProject.Models
+{
+    public class City
+    {
+        public City()
+        {
+            Photos = new List<Photo>(); 
+        }
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public int UserId { get; set; }
+
+        public List<Photo> Photos { get; set; } //NullReferenceExcepiton can be exist(line 7)
+
+        public User User { get; set; }
+    }
+}
